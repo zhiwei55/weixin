45c87dfbf613d0693f3347b2f3299baf3b96c0c6
zhiwei55 <li55_99@126.com>
2015-11-19 23:24:49 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,12 @@
 <?php
+
 define("TOKEN", "weixin");
+
+traceHttp();
+
 $wechatObj = new wechatCallbackapiTest();
-if (isset($_GET['echostr'])) {
-    $wechatObj->valid();
-}else{
-    $wechatObj->responseMsg();
-}
+$wechatObj->valid();
+
 class wechatCallbackapiTest
 {
     public function valid()
@@ -16,50 +17,37 @@ class wechatCallbackapiTest
             exit;
         }
     }
+
     private function checkSignature()
     {
         $signature = $_GET["signature"];
         $timestamp = $_GET["timestamp"];
         $nonce = $_GET["nonce"];
+
         $token = TOKEN;
         $tmpArr = array($token, $timestamp, $nonce);
         sort($tmpArr);
         $tmpStr = implode( $tmpArr );
         $tmpStr = sha1( $tmpStr );
+
         if( $tmpStr == $signature ){
             return true;
         }else{
             return false;
         }
     }
-    public function responseMsg()
-    {
-        $postStr = $GLOBALS["HTTP_RAW_POST_DATA"];
-        if (!empty($postStr)){
-            $postObj = simplexml_load_string($postStr, 'SimpleXMLElement', LIBXML_NOCDATA);
-            $fromUsername = $postObj->FromUserName;
-            $toUsername = $postObj->ToUserName;
-            $keyword = trim($postObj->Content);
-            $time = time();
-            $textTpl = "<xml>
-                        <ToUserName><![CDATA[%s]]></ToUserName>
-                        <FromUserName><![CDATA[%s]]></FromUserName>
-                        <CreateTime>%s</CreateTime>
-                        <MsgType><![CDATA[%s]]></MsgType>
-                        <Content><![CDATA[%s]]></Content>
-                        <FuncFlag>0</FuncFlag>
-                        </xml>";
-            if($keyword == "?" || $keyword == "£¿")
-            {
-                $msgType = "text";
-                $contentStr = date("Y-m-d H:i:s",time());
-                $resultStr = sprintf($textTpl, $fromUsername, $toUsername, $time, $msgType, $contentStr);
-                echo $resultStr;
-            }
-        }else{
-            echo "";
-            exit;
-        }
-    }
+    
+    
 }
+
+function traceHttp()
+{
+    logger("\n\nREMOTE_ADDR:".$_SERVER["REMOTE_ADDR"].(strstr($_SERVER["REMOTE_ADDR"],'101.226')? " FROM WeiXin": "Unknown IP"));
+    logger("QUERY_STRING:".$_SERVER["QUERY_STRING"]);
+}
+function logger($log_content)
+{
+    file_put_contents("log.html",date('Y-m-d H:i:s ').$content."<br>",FILE_APPEND);
+}
+
 ?>
