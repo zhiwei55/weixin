3df1ba86d4288836476f4186df72bf89fac9d517
zhiwei55 <li55_99@126.com>
2015-11-19 23:29:56 +0800

    Create token.php

---
+++ token.php
@@ -0,0 +1,53 @@
+<?php
+
+define("TOKEN", "weixin");
+
+traceHttp();
+
+$wechatObj = new wechatCallbackapiTest();
+$wechatObj->valid();
+
+class wechatCallbackapiTest
+{
+    public function valid()
+    {
+        $echoStr = $_GET["echostr"];
+        if($this->checkSignature()){
+            echo $echoStr;
+            exit;
+        }
+    }
+
+    private function checkSignature()
+    {
+        $signature = $_GET["signature"];
+        $timestamp = $_GET["timestamp"];
+        $nonce = $_GET["nonce"];
+
+        $token = TOKEN;
+        $tmpArr = array($token, $timestamp, $nonce);
+        sort($tmpArr);
+        $tmpStr = implode( $tmpArr );
+        $tmpStr = sha1( $tmpStr );
+
+        if( $tmpStr == $signature ){
+            return true;
+        }else{
+            return false;
+        }
+    }
+    
+    
+}
+
+function traceHttp()
+{
+    logger("\n\nREMOTE_ADDR:".$_SERVER["REMOTE_ADDR"].(strstr($_SERVER["REMOTE_ADDR"],'101.226')? " FROM WeiXin": "Unknown IP"));
+    logger("QUERY_STRING:".$_SERVER["QUERY_STRING"]);
+}
+function logger($log_content)
+{
+    file_put_contents("log.html",date('Y-m-d H:i:s ').$content."<br>",FILE_APPEND);
+}
+
+?>
